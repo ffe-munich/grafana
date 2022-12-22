7448f26de93464a96a93972494315a34e150d6f1
jferstl <121223492+jferstl@users.noreply.github.com>
2022-12-22 17:27:12 +0100

    Panel Dropdown only visible for admins

--- public/app/features/dashboard/dashgrid/PanelHeader/PanelHeader.tsx
+++ public/app/features/dashboard/dashgrid/PanelHeader/PanelHeader.tsx
@@ -1,8 +1,9 @@
 import { css, cx } from '@emotion/css';
-import React, { FC } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 
 import { DataLink, GrafanaTheme2, PanelData } from '@grafana/data';
 import { selectors } from '@grafana/e2e-selectors';
+import { getBackendSrv } from '@grafana/runtime';
 import { Icon, useStyles2, ClickOutsideWrapper } from '@grafana/ui';
 import { DashboardModel } from 'app/features/dashboard/state/DashboardModel';
 import { PanelModel } from 'app/features/dashboard/state/PanelModel';
@@ -33,54 +34,137 @@ export const PanelHeader: FC<Props> = ({ panel, error, isViewing, isEditing, dat
   const className = cx('panel-header', !(isViewing || isEditing) ? 'grid-drag-handle' : '');
   const styles = useStyles2(panelStyles);
 
-  return (
-    <>
-      <PanelHeaderLoadingIndicator state={data.state} onClick={onCancelQuery} />
-      <PanelHeaderCorner
-        panel={panel}
-        title={panel.title}
-        description={panel.description}
-        scopedVars={panel.scopedVars}
-        links={getPanelLinksSupplier(panel)}
-        error={error}
-      />
-      <div className={className}>
-        <PanelHeaderMenuTrigger data-testid={selectors.components.Panels.Panel.title(title)}>
-          {({ closeMenu, panelMenuOpen }) => {
-            return (
-              <ClickOutsideWrapper onClick={closeMenu} parent={document}>
-                <div className="panel-title">
-                  <PanelHeaderNotices frames={data.series} panelId={panel.id} />
-                  {alertState ? (
-                    <Icon
-                      name={alertState === 'alerting' ? 'heart-break' : 'heart'}
-                      className="icon-gf panel-alert-icon"
-                      style={{ marginRight: '4px' }}
-                      size="sm"
-                    />
-                  ) : null}
-                  <h2 className={styles.titleText}>{title}</h2>
-                  {!dashboard.meta.publicDashboardAccessToken && (
-                    <div data-testid="panel-dropdown">
-                      <Icon name="angle-down" className="panel-menu-toggle" />
-                      {panelMenuOpen ? (
-                        <PanelHeaderMenuWrapper panel={panel} dashboard={dashboard} onClose={closeMenu} />
-                      ) : null}
-                    </div>
-                  )}
-                  {data.request && data.request.timeInfo && (
-                    <span className="panel-time-info">
-                      <Icon name="clock-nine" size="sm" /> {data.request.timeInfo}
-                    </span>
-                  )}
-                </div>
-              </ClickOutsideWrapper>
-            );
-          }}
-        </PanelHeaderMenuTrigger>
-      </div>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+  const [isAdmin, setIsAdmin] = useState(undefined);
+
+  const checkAdmin = () => {
+    setLoading(true);
+    getBackendSrv()
+      .get(`/api/user`)
+      .then((res) => {
+        console.log('got the promise!');
+        setLoading(false);
+        setIsAdmin(res['isGrafanaAdmin']);
+      })
+      .catch((err) => {
+        // Error handling
+        console.log('promise failed....');
+        setLoading(false);
+        console.log(err);
+        return null;
+      });
+  };
+
+  useEffect(() => {
+    checkAdmin();
+  }, []);
+
+  if (loading) {
+    return <div></div>;
+  }
+
+  // das zurückgegebene Objekt unterscheidet sich nur in einer Zeile
+  //      --> <div className={styles.panelDropdown} data-testid="panel-dropdown">
+  // ggf. noch einen Weg finden, wie nur dieser Teil basieren auf der Fallunterscheidung geändert werden kann
+  if (isAdmin) {
+    return (
+      <>
+        <PanelHeaderLoadingIndicator state={data.state} onClick={onCancelQuery} />
+        <PanelHeaderCorner
+          panel={panel}
+          title={panel.title}
+          description={panel.description}
+          scopedVars={panel.scopedVars}
+          links={getPanelLinksSupplier(panel)}
+          error={error}
+        />
+        <div className={className}>
+          <PanelHeaderMenuTrigger data-testid={selectors.components.Panels.Panel.title(title)}>
+            {({ closeMenu, panelMenuOpen }) => {
+              return (
+                <ClickOutsideWrapper onClick={closeMenu} parent={document}>
+                  <div className="panel-title">
+                    <PanelHeaderNotices frames={data.series} panelId={panel.id} />
+                    {alertState ? (
+                      <Icon
+                        name={alertState === 'alerting' ? 'heart-break' : 'heart'}
+                        className="icon-gf panel-alert-icon"
+                        style={{ marginRight: '4px' }}
+                        size="sm"
+                      />
+                    ) : null}
+                    <h2 className={styles.titleText}>{title}</h2>
+                    {!dashboard.meta.publicDashboardAccessToken && (
+                      <div data-testid="panel-dropdown">
+                        <Icon name="angle-down" className="panel-menu-toggle" />
+                        {panelMenuOpen ? (
+                          <PanelHeaderMenuWrapper panel={panel} dashboard={dashboard} onClose={closeMenu} />
+                        ) : null}
+                      </div>
+                    )}
+                    {data.request && data.request.timeInfo && (
+                      <span className="panel-time-info">
+                        <Icon name="clock-nine" size="sm" /> {data.request.timeInfo}
+                      </span>
+                    )}
+                  </div>
+                </ClickOutsideWrapper>
+              );
+            }}
+          </PanelHeaderMenuTrigger>
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <PanelHeaderLoadingIndicator state={data.state} onClick={onCancelQuery} />
+        <PanelHeaderCorner
+          panel={panel}
+          title={panel.title}
+          description={panel.description}
+          scopedVars={panel.scopedVars}
+          links={getPanelLinksSupplier(panel)}
+          error={error}
+        />
+        <div className={className}>
+          <PanelHeaderMenuTrigger data-testid={selectors.components.Panels.Panel.title(title)}>
+            {({ closeMenu, panelMenuOpen }) => {
+              return (
+                <ClickOutsideWrapper onClick={closeMenu} parent={document}>
+                  <div className="panel-title">
+                    <PanelHeaderNotices frames={data.series} panelId={panel.id} />
+                    {alertState ? (
+                      <Icon
+                        name={alertState === 'alerting' ? 'heart-break' : 'heart'}
+                        className="icon-gf panel-alert-icon"
+                        style={{ marginRight: '4px' }}
+                        size="sm"
+                      />
+                    ) : null}
+                    <h2 className={styles.titleText}>{title}</h2>
+                    {!dashboard.meta.publicDashboardAccessToken && (
+                      <div className={styles.panelDropdown} data-testid="panel-dropdown">
+                        <Icon name="angle-down" className="panel-menu-toggle" />
+                        {panelMenuOpen ? (
+                          <PanelHeaderMenuWrapper panel={panel} dashboard={dashboard} onClose={closeMenu} />
+                        ) : null}
+                      </div>
+                    )}
+                    {data.request && data.request.timeInfo && (
+                      <span className="panel-time-info">
+                        <Icon name="clock-nine" size="sm" /> {data.request.timeInfo}
+                      </span>
+                    )}
+                  </div>
+                </ClickOutsideWrapper>
+              );
+            }}
+          </PanelHeaderMenuTrigger>
+        </div>
+      </>
+    );
+  }
 };
 
 const panelStyles = (theme: GrafanaTheme2) => {
@@ -101,6 +185,9 @@ const panelStyles = (theme: GrafanaTheme2) => {
       .panel-has-alert & {
         max-width: calc(100% - 54px);
       }
+    `,
+    panelDropdown: css`
+      display: none;
     `,
   };
 };
